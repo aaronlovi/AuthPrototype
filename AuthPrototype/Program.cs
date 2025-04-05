cfb54f34c34c6ca2e1da86773d2dcebada89dd2b
Aaron Lovi <aaronlovi@gmail.com>
2025-04-05 19:05:11 -0400

    Add Serilog for logging to console

--- AuthPrototype/Program.cs
+++ AuthPrototype/Program.cs
@@ -1,31 +1,54 @@
+using System;
 using AuthPrototype.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace AuthPrototype;
 
-public class Program {
-    public static void Main(string[] args) {
-        var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddSingleton(new UsersFileStore("users.txt"));
-        builder.Services.AddHttpClient<TokenService>();
-        builder.Services.AddControllers();
-        builder.Configuration.AddUserSecrets<Program>();
-        var app = builder.Build();
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
 
-        if (app.Environment.IsDevelopment())
+        try
         {
-            app.UseDeveloperExceptionPage();
+            Log.Information("Starting up the application");
+
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Host.UseSerilog();
+
+            builder.Services.AddSingleton(new UsersFileStore("users.txt"));
+            builder.Services.AddHttpClient<TokenService>();
+            builder.Services.AddControllers();
+            builder.Configuration.AddUserSecrets<Program>();
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.MapGet("/", () => $"AuthPrototype in {app.Environment.EnvironmentName} mode");
+
+            app.Run();
         }
-
-        app.UseHttpsRedirection();
-        app.UseAuthorization();
-        app.MapControllers();
-
-        app.MapGet("/", () => $"AuthPrototype in {app.Environment.EnvironmentName} mode");
-
-        app.Run();
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Application start-up failed");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 }
